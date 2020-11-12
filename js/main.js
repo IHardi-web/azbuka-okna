37694c747101c376425d5f13e97cc7dc1bed1958
IHardi-web <ihardi.nobody@gmail.com>
2020-11-12 23:45:11 +0300

    section 7, 8 (another carousel) done

--- js/main.js
+++ js/main.js
@@ -39,5 +39,26 @@ $(document).ready(function () {
         items: 4,
       }
     }
-  })
+  });
+
+  $('.owl-carousel--portfolio').owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    nav: true,
+    dots: false,
+    navText: ['<span class="arrow arrow--small arrow--left"></span>', '<span class="arrow arrow--small arrow--right"></span>'],
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+        nav: true,
+      }
+    }
+  });
 });
